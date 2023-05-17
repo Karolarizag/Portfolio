efc64abbc98e598bde62714f03fdc0edabd7c2e5
karolarizag <karolarizadev@gmail.com>
2023-05-17 22:23:26 +0100

    updated and refactorized

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,23 +1,22 @@
+import content from "../data/content.json"
 import ProjectCard from "../Components/ProjectCard/ProjectCard";
-import { projects } from "../Services/ProjectsService";
 
-const Projects = () => {
+export const Projects = () => {
+
+  const { projects } = content
+
   return (
-     <div class="md:px-20 px-3 pb-10">
-
-      <h1 class="text-6xl text-primary text-center">Projects</h1>
+    <div class="md:px-20 px-3 pb-10">
+      <h1 class="text-6xl text-primary text-center">
+        { projects.title }
+      </h1>
       <div class="flex flex-wrap justify-center space-between md:px-10 lg:px-20 px-5 mt-10">
-
         {
-          projects.map(project => {
+          projects.content.map(project => {
             return <ProjectCard props={project} />
           })
         }
-
       </div>
-
-     </div>  
+    </div>  
     )
 }
-
-export default Projects;
